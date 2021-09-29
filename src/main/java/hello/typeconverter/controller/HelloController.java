b139a1e7256945a5e790dc20fa8387a02c09cefd
양희서/전시개발팀 <victor.yang@musinsa.com>
2021-09-29 20:48:16 +0900

    섹션 10. 스프링 타입 컨버터 실습

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,24 @@
+package hello.typeconverter.controller;
+
+import hello.typeconverter.type.IpPort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+    
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort.getIp() = " + ipPort.getIp());
+        System.out.println("ipPort.getPort() = " + ipPort.getPort());
+        return "ok";
+    }
+
+}
---
+++ src/test/java/hello/converter/ConversionServiceTest.java
@@ -0,0 +1,34 @@
+package hello.converter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import hello.typeconverter.converter.IntegerToStringConverter;
+import hello.typeconverter.converter.IpPortToStringConverter;
+import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.converter.StringToIpPortConverter;
+import hello.typeconverter.type.IpPort;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        //등록
+        DefaultConversionService conversionService = new DefaultConversionService();
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        //사용
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+
+        IpPort result = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        assertThat(result).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        String result2 = conversionService.convert(result, String.class);
+        assertThat(result2).isEqualTo("127.0.0.1:8080");
+    }
+}
